74ef409a59e0c0938458f595230f470104a0efd8
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-22 12:57:06 -0300

    Inserir estrutura basica de Jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { useState, useEffect } from 'react'
+
 import { FaAngleDoubleRight } from 'react-icons/fa'
+
+
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
+
+
+
 function App() {
-  return <h2>tabs project setup</h2>
-}
+  const [ loading, setloading ] = useState(true);
+
+
+  const renderComponents = () => {
+    if (loading) {
+      return (
+        <section className="section loading">
+          <h1>loading...</h1>
+        </section>
+      );
+    }
+  };
+
+  return (
+    <main>
+      {renderComponents()}
+    </main>
+  );
+};
+
+
 
 export default App
+
+
